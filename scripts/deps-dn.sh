a6e1adb705a284e39a402a02bbcc1dbf9d23a42c
Michael Penhallegon <mike@hematite.tech>
2022-07-10 13:56:19 -0700

    setup basic configuration

--- scripts/deps-dn.sh
+++ scripts/deps-dn.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set +x
 
-# create network if needed
-podman network create rs_crud_actix-network
-
-# create mongo server
-podman pod create \
-    --network=rs_crud_actix-network \
-    -n prometheus \
-    -p 27017:27017
-
-podman run -dt --pod mongo \
-    -v ./data/mongodb:/data/db:z \
-    docker.io/mongo:3
+if command -v "podman"; then
+    source scripts/podman-deps-dn.sh
+else
+    docker-compose dn
+fi
--- scripts/deps-up.sh
+++ scripts/deps-up.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set +x
 
-# create network if needed
-podman network create rs_crud_actix-network
-
-# create mongo server
-podman pod create \
-    --network=rs_crud_actix-network \
-    -n prometheus \
-    -p 9090:9090
-
-podman run -dt --pod prometheus \
-    -v ./scripts/data/prometheus:/opt/bitnami/prometheus/data:z \
-    docker.io/bitnami/prometheus
+if command -v "podman"; then
+    source scripts/podman-deps-up.sh
+else
+   docker-compose -f scripts/docker-compose.yml up
+fi
